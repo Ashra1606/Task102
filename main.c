fab774abb00f92b7d07fc7059c4e7ddbeb2bc4d4
Ashra1606 <your-email@example.com>
2026-03-27 17:11:35 +0600

    Task102: Fixed the growing circle code

--- main.c
+++ main.c
@@ -1,48 +1,70 @@
-#include<SDL3/SDL.h>
-#include<stdio.h>
-#include<math.h>
+#include <SDL3/SDL.h>
+#include <math.h>
+#include <stdbool.h>
+
 void drawCircle(SDL_Renderer* renderer,int xcnt,int ycnt,int radius)
 {
-	for(int w=0;w<radius*2;w++)
-	{
-		for(int h=0;h<radius*2;h++)
-		{
-			int x=radius-w;
-			int y=radius-h;
-			if((x*x+y*y)<=(radius*radius))
-			{
-				SDL_RenderPoint(renderer,xcnt+x,ycnt+y);
-			}
-		}
-	}
+    for(int w=-radius;w<radius;w++)
+    {
+        for(int h=-radius;h<=radius;h++)
+        {
+            if(w*w+h*h<=radius*radius)
+            {
+                SDL_RenderPoint(renderer,xcnt+w,ycnt+h);
+            }
+        }
+    }
 }
+
 int main()
 {
-	SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO);
 
-	int width=800;
-	int height=600;
+    int width=800;
+    int height=600;
 
-	SDL_Window* window=SDL_CreateWindow("Draw a circle",width,height,0);
-	SDL_Renderer* renderer=SDL_CreateRenderer(window,NULL);
+    int radius=2;
+    int maxradius=2;
 
-	int xcnt=width/2;
-	int ycnt=height/2;
+    int xcnt=width/2;
+    int ycnt=height/2;
 
-	int radius=100;
+    SDL_Window* window=SDL_CreateWindow("Growing Circle", width, height, 0);
+    SDL_Renderer* renderer=SDL_CreateRenderer(window, NULL);
 
-	SDL_SetRenderDrawColor(renderer,0, 128, 0,255);
-	 SDL_RenderClear(renderer);
+    bool ok=true;
+    SDL_Event event;
 
-	 SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	 drawCircle(renderer,xcnt,ycnt,radius);
+    while(ok)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type==SDL_EVENT_QUIT)
+            {
+                ok=false;
+            }
+        }
 
-	 SDL_RenderPresent(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
+        SDL_RenderClear(renderer);
 
-	 SDL_Delay(10000);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        drawCircle(renderer,xcnt,ycnt,radius);
 
-	 SDL_DestroyRenderer(renderer);
-	 SDL_DestroyWindow(window);
-	 SDL_Quit();
+        SDL_RenderPresent(renderer);
 
+        radius+=2;
+        if(xcnt+radius>=width||xcnt-radius<=0||ycnt+radius>=height||ycnt-radius<=0)
+        {
+            radius=maxradius;
+        }
+
+        SDL_Delay(10);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
